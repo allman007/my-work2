5becaead61a96a9778d544c7a33c17fbbaa5f925
allman007 <nwanjiuche@gmail.com>
2020-03-13 14:47:11 +0100

    first commit

---
+++ src/build/precache-manifest.6de86eca71313d3eb279d40f34634c2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2cdf2c126390bf327305ef7155b940d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d1bcd4e31490c0fef5f",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "7980d95b4a9f2de0d5b7",
+    "url": "/static/js/2.181248af.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.181248af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d1bcd4e31490c0fef5f",
+    "url": "/static/js/main.1655511c.chunk.js"
+  },
+  {
+    "revision": "3f87b9e2b4300a74900e",
+    "url": "/static/js/runtime-main.82020a80.js"
+  }
+]);
